02b58be1543347020583f83c4d2580cbe44db588
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-05-18 22:55:16 -0400

    Building to Display a Photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,15 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
+const App = () => {
+
+const photos = [<PhotoListItem key="1"/>]
+
+  return (
+    <div className="App">
+      {photos}
+    </div>
+  )
+}
 
 export default App
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 
 import '../styles/PhotoListItem.scss';
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return (
+    <div >
+      <img src={props.imageSource} alt="" />
+    </div>
+  )
 }
 
 PhotoListItem.defaultProps = {
