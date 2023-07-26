dd7336a349c09f1ede1d61388e99f2896e5d73d2
saed <saedabdi3@gmail.comm>
2023-07-26 13:47:28 -0400

    app.jx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
+import useApplicationData from './hooks/useApplicationData'; 
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
+
 const App = () => {
+  const {
+    photos,
+    topics,
+    likes,
+    openModal,
+    closeModal,
+    detailPhotoId,
+    fetchTopicPhotos,
+    favoritePhoto,
+    showPhoto
+  } = useApplicationData(); 
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <HomeRoute         
+        photos={photos}
+        topics={topics} 
+        likes={likes} 
+        fetchTopicPhotos={fetchTopicPhotos}
+        favoritePhoto={favoritePhoto}
+        openModal={openModal} 
+      />
+      {
+        showPhoto && detailPhotoId && <PhotoDetailsModal 
+                                        photos={photos}
+                                        likes={likes}
+                                        openModal={openModal} 
+                                        closeModal={closeModal}
+                                        favoritePhoto={favoritePhoto}
+                                        detailPhotoId={detailPhotoId}                         
+                                      /> 
+      }
     </div>
-  );
-};
+  )
+  
+}
 
-export default App;
+export default App
