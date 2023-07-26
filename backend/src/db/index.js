8837c71d39afe10501d855c114d3a5fc64913e02
saed <saedabdi3@gmail.comm>
2023-07-26 13:04:02 -0400

    test

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,16 +1,15 @@
-const pg = require("pg");
+const { Pool } = require('pg');
 
-const client = new pg.Client({
-  host: process.env.PGHOST,
-  name: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-});
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+};
 
-client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+const db = new Pool(dbParams);
 
-module.exports = client;
+db.connect();
+
+module.exports = db;
